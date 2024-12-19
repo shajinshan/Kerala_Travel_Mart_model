ff6c43f56952c7d492a1684fcb131cd5c703a29b
shajinshan <shajinshajin02@gmail.com>
2024-12-19 14:05:04 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/kerala_travel_mart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kerala_travel_mart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEDD14),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              height: 90,
+              width: double.infinity,
+              child: Image.asset(
+                "assets/images/ktm-topbar-icon.png",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                ),
+                Container(
+                  child: Image.asset("assets/images/KTM-logo.png"),
+                  height: 120,
+                  width: 200,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        offset: const Offset(3, 4),
+                        spreadRadius: 3,
+                        blurRadius: 5,
+                      )
+                    ],
+                    color: const Color.fromARGB(255, 143, 36, 219),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        "Choose Your Login Type",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Buyer",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(6))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Seller",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(6))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Media",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(6))),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  child: const Column(
+                    children: [
+                      Text(
+                        "Powered By :",
+                        style: TextStyle(fontWeight: FontWeight.w300),
+                      )
+                    ],
+                  ),
+                  height: 120,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          offset: const Offset(2, 3),
+                          blurRadius: 4,
+                          spreadRadius: 2)
+                    ],
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20)),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight *0.12, // Use relative spacing
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      width: 110,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            Icon(
+                              Icons.skip_next,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Skip",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 14),
+                            ),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6))),
+                      ),
+                    ),
+                    SizedBox(
+                      width:
+                          screenWidth * 0.05,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
